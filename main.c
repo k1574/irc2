37236bcfaef9b23e73454cca702f8d15817f4f88
k1574 <k1574@yandex.ru>
2021-02-09 16:36:51 +0500

    Reinit.

---
+++ main.c
@@ -0,0 +1,313 @@
+ /* See "license" file for license details. */
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "arg.h"
+#include "config.h"
+
+char *argv0;
+static char *host = DEFAULT_HOST;
+static char *port = DEFAULT_PORT;
+static char *password;
+static char nick[32];
+static char bufin[4096];
+static char bufout[4096];
+static char channel[256];
+static time_t trespond;
+static FILE *srv;
+
+/*
+ * Copy src to string dst of size siz. At most siz-1 characters
+ * will be copied. Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+#undef strlcpy
+size_t
+strlcpy(char *dst, const char *src, size_t siz)
+{
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	/* Copy as many bytes as will fit */
+	if (n != 0) {
+		while (--n != 0) {
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
+	}
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (siz != 0)
+			*d = '\0'; /* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+	return(s - src - 1); /* count does not include NUL */
+}
+
+static void
+eprint(const char *fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	vsnprintf(bufout, sizeof bufout, fmt, ap);
+	va_end(ap);
+	fprintf(stderr, "%s", bufout);
+	if(fmt[0] && fmt[strlen(fmt) - 1] == ':')
+		fprintf(stderr, " %s\n", strerror(errno));
+	exit(1);
+}
+
+static int
+dial(char *host, char *port) {
+	static struct addrinfo hints;
+	int srv;
+	struct addrinfo *res, *r;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	if(getaddrinfo(host, port, &hints, &res) != 0)
+		eprint("error: cannot resolve hostname '%s':", host);
+	for(r = res; r; r = r->ai_next) {
+		if((srv = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1)
+			continue;
+		if(connect(srv, r->ai_addr, r->ai_addrlen) == 0)
+			break;
+		close(srv);
+	}
+	freeaddrinfo(res);
+	if(!r)
+		eprint("error: cannot connect to host '%s'\n", host);
+	return srv;
+}
+
+static char *
+eat(char *s, int (*p)(int), int r) {
+	while(*s != '\0' && p(*s) == r)
+		s++;
+	return s;
+}
+
+static char*
+skip(char *s, char c) {
+	while(*s != c && *s != '\0')
+		s++;
+	if(*s != '\0')
+		*s++ = '\0';
+	return s;
+}
+static void
+trim(char *s) {
+	char *e;
+
+	e = s + strlen(s) - 1;
+	while(isspace(*e) && e > s)
+		e--;
+	*(e + 1) = '\0';
+}
+
+static void
+pout(char *channel, char *fmt, ...) {
+	static char timestr[80];
+	time_t t;
+	va_list ap;
+
+	va_start(ap, fmt);
+	vsnprintf(bufout, sizeof bufout, fmt, ap);
+	va_end(ap);
+	t = time(NULL);
+	strftime(timestr, sizeof timestr, TIMESTAMP_FORMAT, localtime(&t));
+	fprintf(stdout, "%-12s : %s %s\n", channel, timestr, bufout);
+}
+
+static void
+sout(char *fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	vsnprintf(bufout, sizeof bufout, fmt, ap);
+	va_end(ap);
+	fprintf(srv, "%s\r\n", bufout);
+}
+
+static void
+privmsg(char *channel, char *msg) {
+	if(channel[0] == '\0') {
+		pout("", "No channel to send to");
+		return;
+	}
+	pout(channel, "<%s> %s", nick, msg);
+	sout("PRIVMSG %s :%s", channel, msg);
+}
+
+static void
+parsein(char *s) {
+	char c, *p;
+
+	if(s[0] == '\0')
+		return;
+	skip(s, '\n');
+	if(s[0] != COMMAND_PREFIX_CHARACTER) {
+		privmsg(channel, s);
+		return;
+	}
+	c = *++s;
+	if(c != '\0' && isspace(s[1])) {
+		p = s + 2;
+		switch(c) {
+		case 'j':
+			sout("JOIN %s", p);
+			if(channel[0] == '\0')
+				strlcpy(channel, p, sizeof channel);
+			return;
+		case 'l':
+			s = eat(p, isspace, 1);
+			p = eat(s, isspace, 0);
+			if(!*s)
+				s = channel;
+			if(*p)
+				*p++ = '\0';
+			if(!*p)
+				p = DEFAULT_PARTING_MESSAGE;
+			sout("PART %s :%s", s, p);
+			return;
+		case 'm':
+			s = eat(p, isspace, 1);
+			p = eat(s, isspace, 0);
+			if(*p)
+				*p++ = '\0';
+			privmsg(s, p);
+			return;
+		case 's':
+			strlcpy(channel, p, sizeof channel);
+			return;
+		}
+	}
+	sout("%s", s);
+}
+
+static void
+parsesrv(char *cmd) {
+	char *usr, *par, *txt;
+
+	usr = host;
+	if(!cmd || !*cmd)
+		return;
+	if(cmd[0] == ':') {
+		usr = cmd + 1;
+		cmd = skip(usr, ' ');
+		if(cmd[0] == '\0')
+			return;
+		skip(usr, '!');
+	}
+	skip(cmd, '\r');
+	par = skip(cmd, ' ');
+	txt = skip(par, ':');
+	trim(par);
+	if(!strcmp("PONG", cmd))
+		return;
+	if(!strcmp("PRIVMSG", cmd))
+		pout(par, "<%s> %s", usr, txt);
+	else if(!strcmp("PING", cmd))
+		sout("PONG %s", txt);
+	else {
+		pout(usr, ">< %s (%s): %s", cmd, par, txt);
+		if(!strcmp("NICK", cmd) && !strcmp(usr, nick))
+			strlcpy(nick, txt, sizeof nick);
+	}
+}
+
+
+static void
+usage(void) {
+	eprint("usage: %s [-h host] [-p port] [-n nick] [-k keyword] [-v]\n", argv0);
+}
+
+void
+main(int argc, char *argv[]) {
+	struct timeval tv;
+	const char *user = getenv("USER");
+	int n;
+	fd_set rd;
+
+	strlcpy(nick, user ? user : "unknown", sizeof nick);
+	ARGBEGIN {
+	case 'h':
+		host = EARGF(usage());
+		break;
+	case 'p':
+		port = EARGF(usage());
+		break;
+	case 'n':
+		strlcpy(nick, EARGF(usage()), sizeof nick);
+		break;
+	case 'k':
+		password = EARGF(usage());
+		break;
+	case 'v':
+		eprint("%s-"VERSION", © 2005-2014 Kris Maglione, Anselm R. Garbe, Nico Golde\n", argv0);
+		break;
+	default:
+		usage();
+	} ARGEND;
+
+	/* init */
+	srv = fdopen(dial(host, port), "r+");
+	if (!srv)
+		eprint("fdopen:");
+	/* login */
+	if(password)
+		sout("PASS %s", password);
+	sout("NICK %s", nick);
+	sout("USER %s localhost %s :%s", nick, host, nick);
+	fflush(srv);
+	setbuf(stdout, NULL);
+	setbuf(srv, NULL);
+	setbuf(stdin, NULL);
+#ifdef __OpenBSD__
+	if (pledge("stdio", NULL) == -1)
+		eprint("error: pledge:");
+#endif
+	for(;;) { /* main loop */
+		FD_ZERO(&rd);
+		FD_SET(0, &rd);
+		FD_SET(fileno(srv), &rd);
+		tv.tv_sec = 120;
+		tv.tv_usec = 0;
+		n = select(fileno(srv) + 1, &rd, 0, 0, &tv);
+		if(n < 0) {
+			if(errno == EINTR)
+				continue;
+			eprint("%s: error on select():", argv0);
+		}
+		else if(n == 0) {
+			if(time(NULL) - trespond >= 300)
+				eprint("%s: shutting down: parse timeout\n", argv0);
+			sout("PING %s", host);
+			continue;
+		}
+		if(FD_ISSET(fileno(srv), &rd)) {
+			if(fgets(bufin, sizeof bufin, srv) == NULL)
+				eprint("%s: remote host closed connection\n", argv0);
+			parsesrv(bufin);
+			trespond = time(NULL);
+		}
+		if(FD_ISSET(0, &rd)) {
+			if(fgets(bufin, sizeof bufin, stdin) == NULL)
+				eprint("%s: broken pipe\n", argv0);
+			parsein(bufin);
+		}
+	}
+	exit(0);
+}
